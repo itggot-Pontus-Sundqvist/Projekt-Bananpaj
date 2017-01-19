7b7179f622396e986344cafbe9aad1fb554c1dd4
itggot-Pontus-Sundqvist <pontus.sundqvist@itggot.se>
2017-01-19 10:46:46 +0100

    Added menu, css and javascript

---
+++ site/js/functions.js
@@ -0,0 +1,8 @@
+function slide(){
+  var element = document.getElementById("navi");
+  element.classList.toggle("menu");
+  element.classList.toggle("menuOut");
+  var element = document.getElementById("arr");
+  element.classList.toggle("arrow");
+  element.classList.toggle("arrowOut");
+}
